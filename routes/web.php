4c7ea5d1ced62c3972e94a31df5b6ddf139e4547
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-14 16:59:23 +0500

    feat: Routes for album added in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AlbumController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/albums', [AlbumController::class, 'index'])->name('albums.index');
+Route::get('/albums/create', [AlbumController::class, 'create'])->name('albums.create');
+Route::post('/albums', [AlbumController::class, 'store'])->name('albums.store');
+Route::get('/albums/{album}/edit', [AlbumController::class, 'edit'])->name('albums.edit');
+Route::put('/albums/{album}', [AlbumController::class, 'update'])->name('albums.update');
+Route::delete('/albums/{album}', [AlbumController::class, 'destroy'])->name('albums.destroy');
